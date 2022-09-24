e1115df568cdbead9f6c2be638ace0fa8bb68cd5
abc7989 <114270414+abc7989@users.noreply.github.com>
2022-09-24 18:20:41 +1200

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,11 +21,11 @@ panelConfig() {
   #read -r -p "Enter panel domain(Include https:// or http://): " pUrl
   #read -r -p "Enter panel token: " nKey
   #read -r -p "Enter node_ids, (eg 1,2,3): " nIds
-  echo && echo -e "Choose panel type:
-  1. SSPanel
-  2. V2board
-  3. Django-sspanel"
-  read -r -p "Choose panel type: " panelnum
+  #echo && echo -e "Choose panel type:
+  #1. SSPanel
+  #2. V2board
+  #3. Django-sspanel"
+  #read -r -p "Choose panel type: " panelnum
   if [ "$panelnum" == "1" ]; then
     panelType="sspanel"
   fi
